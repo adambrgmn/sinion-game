da2c263d83b9a22227008bd30f66647a08ca8f47
Adam Bergman <adam@fransvilhelm.com>
2016-08-19 16:25:19 +0200

    Initialized Progress component

---
+++ app/components/Progress/Progress.spec.js
@@ -0,0 +1,11 @@
+import { expect } from 'chai';
+import { shallow } from 'enzyme';
+import React from 'react';
+
+import Progress from '../Progress';
+import styles from './styles.scss';
+
+describe('Component: <Progress>', () => {
+  it('should render a paragraph of completed and level');
+  it('should update when props is updated');
+});
