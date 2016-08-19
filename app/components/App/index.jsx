365fac8967cf2c83f8ec223ee4abef280beca57d
Adam Bergman <adam@fransvilhelm.com>
2016-08-19 12:37:25 +0200

    Minor spelling

--- app/components/App/index.jsx
+++ app/components/App/index.jsx
@@ -6,7 +6,7 @@ export default function App() {
   return (
     <div className={styles.appContainer}>
       <div className={styles.message}>
-        <p className={styles.messageText}>Let's get started</p>
+        <p className={styles.messageText}>Let's get started!</p>
       </div>
 
       <div className={styles.menu}>
