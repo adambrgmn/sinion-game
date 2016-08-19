f64eeeeb227e21240249dd8c507ae34ac8c6cb6f
Adam Bergman <adam@fransvilhelm.com>
2016-08-19 11:57:26 +0200

    New structure

--- app/components/App/index.jsx
+++ app/components/App/index.jsx
@@ -5,7 +5,20 @@ import styles from './styles.scss';
 export default function App() {
   return (
     <div className={styles.appContainer}>
-      <p>Hello!</p>
+      <div className={styles.message}>
+        <p className={styles.messageText}>Let's get started</p>
+      </div>
+
+      <div className={styles.menu}>
+        <button className={styles.menuButton}>Start game</button>
+      </div>
+
+      <div className={styles.game}>
+        <button className={styles.gameButton}>1</button>
+        <button className={styles.gameButton}>2</button>
+        <button className={styles.gameButton}>3</button>
+        <button className={styles.gameButton}>4</button>
+      </div>
     </div>
   );
 }
