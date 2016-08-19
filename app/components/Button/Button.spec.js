37700636a5001e3195133233eaada1d21da3c19a
Adam Bergman <adam@fransvilhelm.com>
2016-08-19 15:59:26 +0200

    Updated button

--- app/components/Button/Button.spec.js
+++ app/components/Button/Button.spec.js
@@ -1,6 +1,6 @@
 import { expect } from 'chai';
 import { shallow } from 'enzyme';
-import sinon from 'sinon';
+import { spy } from 'sinon';
 import React from 'react';
 
 import Button from '../Button';
@@ -15,7 +15,7 @@ describe('Component: <Button>', () => {
   });
 
   it('should fire a function when clicked', () => {
-    const onButtonClick = sinon.spy();
+    const onButtonClick = spy();
     const wrapper = shallow(<Button onButtonClick={onButtonClick} />);
     wrapper.find(`.${styles.button}`).simulate('click');
 
@@ -24,7 +24,7 @@ describe('Component: <Button>', () => {
 
   it('should render children, when passed', () => {
     const child = 'Text';
-    const wrapper = shallow(<Button>child</Button>);
+    const wrapper = shallow(<Button>{child}</Button>);
     const found = wrapper.contains(child);
 
     expect(found).to.equal(true);
@@ -36,5 +36,18 @@ describe('Component: <Button>', () => {
     const foundButton = wrapper.find(`.${className}`);
 
     expect(foundButton).to.have.length(1);
+  });
+
+  it('should apply a "active"-class on mouseDown', () => {
+    const wrapper = shallow(<Button />);
+    const button = wrapper.find(`.${styles.button}`);
+
+    button.simulate('mouseDown');
+    let found = wrapper.find(`.${styles.buttonActive}`);
+    expect(found).to.have.length(1);
+
+    button.simulate('mouseUp');
+    found = wrapper.find(`.${styles.buttonActive}`);
+    expect(found).to.have.length(0);
   });
 });
--- app/components/Button/index.jsx
+++ app/components/Button/index.jsx
@@ -1,33 +1,58 @@
-import React, { PropTypes } from 'react';
+import React, { Component, PropTypes } from 'react';
 import classNames from 'classnames';
 
 import styles from './styles.scss';
 
-export default function Button({
-  onButtonClick,
-  customClassName,
-  children,
-}) {
-  const cx = {
-    [styles.button]: true,
-  };
+export default class Button extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      active: false,
+    };
 
-  return (
-    <button
-      className={classNames(cx, customClassName)}
-      onClick={onButtonClick}
-    >
-      {children}
-    </button>
-  );
+    this.onMouseDown = this.onMouseDown.bind(this);
+    this.onMouseUp = this.onMouseUp.bind(this);
+  }
+
+  onMouseDown() { this.setState({ active: true }); }
+
+  onMouseUp() { this.setState({ active: false }); }
+
+  render() {
+    const {
+      active,
+      onButtonClick,
+      customClassName,
+      children,
+    } = this.props;
+
+    const cx = {
+      [styles.button]: true,
+      [styles.buttonActive]: this.state.active || active,
+    };
+
+    return (
+      <button
+        className={classNames(cx, customClassName)}
+        onClick={onButtonClick}
+        onMouseDown={this.onMouseDown}
+        onMouseUp={this.onMouseUp}
+      >
+        {children}
+      </button>
+    );
+  }
 }
 
 Button.propTypes = {
+  active: PropTypes.bool,
   onButtonClick: PropTypes.func,
+  onButtonMouseDown: PropTypes.func,
+  onButtonMouseUp: PropTypes.func,
   customClassName: PropTypes.oneOfType([
     PropTypes.string,
     PropTypes.array,
     PropTypes.object,
   ]),
-  children: PropTypes.string,
+  children: PropTypes.any,
 };
