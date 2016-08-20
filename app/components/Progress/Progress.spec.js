78feb9adb8623345e83fcaeeb70c6a73ad540c97
Adam Bergman <adam@fransvilhelm.com>
2016-08-20 21:56:00 +0200

    Writing unit tests for component Progress

--- app/components/Progress/Progress.spec.js
+++ app/components/Progress/Progress.spec.js
@@ -5,7 +5,28 @@ import React from 'react';
 import Progress from '../Progress';
 import styles from './styles.scss';
 
+import Paragraph from '../Paragraph';
+
 describe('Component: <Progress>', () => {
-  it('should render a paragraph of completed and level');
+  it('should render a paragraph of completed and level', () => {
+    const wrapper = shallow(<Progress />);
+    const found = wrapper.find(Paragraph);
+
+    expect(found).to.have.length(1);
+  });
+
+  it('should print out players current and computer total', () => {
+    const props = {
+      playerCurrent: 2,
+      computerTotal: 10,
+    };
+    const wrapper = shallow(<Progress {...props} />);
+    const foundPlayer = wrapper.contains(props.playerCurrent);
+    const foundComputer = wrapper.contains(props.playerTotal);
+
+    expect(foundPlayer).to.equal(true);
+    expect(foundComputer).to.equal(true);
+  });
+
   it('should update when props is updated');
 });
