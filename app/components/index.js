446f266ae24bc8f59d70567ab6e358d2680e5273
Adam Bergman <adam@fransvilhelm.com>
2016-08-22 12:16:50 +0200

    Added root component exporter

---
+++ app/components/index.js
@@ -0,0 +1,6 @@
+export Button from './Button';
+export Game from './Game';
+export Menu from './Menu';
+export Message from './Message';
+export Paragraph from './Paragraph';
+export Progress from './Progress';
