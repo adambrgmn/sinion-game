2e398770c6f222f543079eee5b283fe80f92e5d0
Adam Bergman <adam@fransvilhelm.com>
2016-08-22 16:41:31 +0200

    Continued development
    Still trying to fix flow of game

--- app/containers/App/index.jsx
+++ app/containers/App/index.jsx
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
+import * as config from '../../config';
 
 import styles from './styles.scss';
+
+import randomInt from '../../utils/randomInt';
 
 import {
   Menu,
@@ -16,6 +19,9 @@ export default class App extends Component {
       computerSeq: [],
       playerSeq: [],
       gameRunning: false,
+      computerPlaying: false,
+      activeButton: undefined,
+      error: false,
       message: 'Let\'s get started! (Click the button 👆)',
       progress: {
         computerTotal: 0,
@@ -24,16 +30,114 @@ export default class App extends Component {
     };
   }
 
-  onMenuButtonClick = () => this.setState({ gameRunning: !this.state.gameRunning }, () => {
-    this.updateMessage('Okey, great. I will go first and you just follow along...');
-  })
+  onMenuButtonClick = () => {
+    const { gameRunning } = this.state;
 
-  updateMessage = (msg) => this.setState({ message: msg })
+    // Check if game is running
+    if (gameRunning) {
+      // If game is running, reset the game plan and everything with it
+      // The restart and let the computer play a new sequence
+      this.setState({
+        gameRunning: !gameRunning,
+        message: 'So you wan\'t to go again? Okey, I go first...',
+        computerSeq: [],
+        progress: {
+          computerTotal: 0,
+          playerCurrent: 0,
+        },
+      }, this.playComputerRound);
+    } else {
+      // Else, set state to game running and let the the computer play a sequence
+      this.setState({
+        gameRunning: !gameRunning,
+        message: 'Okey, great. I will go first and you just follow along...',
+      }, this.playComputerRound);
+    }
+  }
+
+  onPlayerButtonPress = (clickedTile) => {
+    const {
+      computerSeq,
+      playerSeq,
+    } = this.state;
+
+    const playerSeqLength = playerSeq.length;
+    const correspondingComputerTile = computerSeq[playerSeqLength];
+
+    if (correspondingComputerTile !== clickedTile) {
+      return this.setState({
+        playerSeq: [],
+        error: true,
+        message: 'Gaah! Sorry that was wrong! Wan\'t to go at it again? (Click the button 👆)',
+      });
+    }
+
+    return this.setState({
+      playerSeq: playerSeq.concat(clickedTile),
+      message: 'Great, you\'re doing fine. Just keep it going!',
+      progress: {
+        computerTotal: this.state.computerSeq.length,
+        playerCurrent: this.state.progress.playerCurrent + 1,
+      },
+    }, () => {
+      if (this.state.playerSeq.length === this.state.computerSeq.length) {
+        this.playComputerRound();
+      }
+    });
+  }
+
+  playComputerRound = () => {
+    // Keep track of how many times the interval have been called
+    let intervalCount = 0;
+    const timeDiff = config.computerActiveButton;
+
+    // Add a new tile to the sequence and set computerPlaying to true
+    this.setState(() => {
+      const newTile = randomInt(1, 4);
+      return {
+        computerSeq: this.state.computerSeq.concat(newTile),
+        computerPlaying: true,
+      };
+    });
+
+    // Instatiate the sequence
+    const interval = setInterval(() => {
+      const { computerSeq } = this.state;
+
+      // If the there are still tiles left in the sequence, fetch them,
+      // play a sound and activate a button.
+      // Then add one call to the interval
+      if (intervalCount < computerSeq.length) {
+        this.activateButtonComputer(computerSeq[intervalCount]);
+        intervalCount++;
+      } else {
+        // If there are no more tiles, clear the interval and prompt the user to
+        // do exactly the same
+        this.setState({
+          computerPlaying: false,
+          message: 'Now it\'s your turn. Do EXCACTLY as i did!',
+          progress: {
+            computerTotal: this.state.computerSeq.length,
+            playerCurrent: 0,
+          },
+        });
+        clearInterval(interval);
+      }
+    }, timeDiff);
+  }
+
+  activateButtonComputer = (tile) => {
+    const timeDiff = config.computerActiveButton / 4 * 3;
+
+    this.setState({ activeButton: tile });
+    setTimeout(() => this.setState({ activeButton: undefined }), timeDiff);
+  }
 
   render() {
     const {
       gameRunning,
       message,
+      activeButton,
       progress,
     } = this.state;
 
@@ -41,7 +145,7 @@ export default class App extends Component {
       <div className={styles.appContainer}>
         <Menu gameRunning={gameRunning} onMenuButtonClick={this.onMenuButtonClick} />
         <Message message={message} />
-        <Game /> {/* activeButton, onButtonClick(num) */}
+        <Game activeButton={activeButton} onButtonClick={this.onPlayerButtonPress} />
         <Progress {...progress} />
       </div>
     );
