6ca6b25b7871696c34ac35fed667e1cf3b1ee370
Adam Bergman <adam@fransvilhelm.com>
2016-08-23 21:24:09 +0200

    Updated Menu to include Settings button

--- app/components/Menu/Menu.spec.js
+++ app/components/Menu/Menu.spec.js
@@ -9,13 +9,13 @@ import styles from './styles.scss';
 import Button from '../Button';
 
 describe('Component: <Menu>', () => {
-  it('should render a menu containing a <Button>', () => {
+  it('should render a menu containing two <Button>', () => {
     const wrapper = shallow(<Menu />);
     const menu = wrapper.find(`.${styles.menu}`);
     const button = wrapper.find(Button);
 
     expect(menu).to.have.length(1);
-    expect(button).to.have.length(1);
+    expect(button).to.have.length(2);
   });
 
   it('should render different messages depending on game state', () => {
@@ -30,10 +30,17 @@ describe('Component: <Menu>', () => {
 
   it('should fire a function when button is clicked', () => {
     const onButtonClick = spy();
-    const wrapper = mount(<Menu onMenuButtonClick={onButtonClick} />);
-    const button = wrapper.find(`.${styles.menuButton}`);
+    const wrapper = mount(<Menu onNewGameButtonClick={onButtonClick} />);
+    const button = wrapper.find(`.${styles.menuButton}`).first();
     button.simulate('click');
 
     expect(onButtonClick.calledOnce).to.equal(true);
+  });
+
+  it('should accept and render children', () => {
+    const wrapper = shallow(<Menu><div className="child">Hello</div></Menu>);
+    const child = wrapper.find('.child');
+
+    expect(child).to.have.length(1);
   });
 });
--- app/components/Menu/index.jsx
+++ app/components/Menu/index.jsx
@@ -7,7 +7,9 @@ import styles from './styles.scss';
 
 export default function Menu({
   gameRunning,
-  onMenuButtonClick,
+  onNewGameButtonClick,
+  onSettingsButtonClick,
+  children,
 }) {
   const cx = {
     [styles.menu]: true,
@@ -17,15 +19,24 @@ export default function Menu({
     <div className={classNames(cx)}>
       <Button
         customClassName={styles.menuButton}
-        onButtonClick={onMenuButtonClick}
+        onButtonClick={onNewGameButtonClick}
       >
         {gameRunning ? 'Reset game' : 'Start new game'}
       </Button>
+      <Button
+        customClassName={styles.menuButton}
+        onButtonClick={onSettingsButtonClick}
+      >
+        Settings
+      </Button>
+      {children}
     </div>
   );
 }
 
 Menu.propTypes = {
   gameRunning: PropTypes.bool,
-  onMenuButtonClick: PropTypes.func,
+  onNewGameButtonClick: PropTypes.func,
+  onSettingsButtonClick: PropTypes.func,
+  children: PropTypes.element,
 };
