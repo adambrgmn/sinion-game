8407cd2b5afcc717f1113a8a5ac43e90738c6310
Adam Bergman <adam@fransvilhelm.com>
2016-08-23 21:25:39 +0200

    Building Settings component

---
+++ app/components/Settings/Settings.spec.js
@@ -0,0 +1,12 @@
+import { expect } from 'chai';
+import { shallow, mount } from 'enzyme';
+import React from 'react';
+
+import Settings from '../Settings';
+import styles from './styles.scss';
+
+describe('Component: <Settings>', () => {
+  it('should render a settings container and a few switches');
+  it('should react when a switch is toggled');
+  it('should react when exit button is clicked');
+});
